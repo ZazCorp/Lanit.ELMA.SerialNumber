38f50198f86ad30d3f914f8382785f9e53f3b181
Zazimko <Zazimkoav@gmail.com>
2017-04-11 13:06:58 +0300

    Тестовый проект. Создание модуля для порядковых номеров.

---
+++ SerialNumber/SerialNumber/Components/SerailNumberBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SerialNumber.Components
+{
+    public class SerialNumberBase
+    {
+        /// <summary>
+        /// Экземпляр класса
+        /// </summary>
+        private static SerialNumberBase _instance;
+        /// <summary>
+        /// Порядковый номер
+        /// </summary>
+        public int SerialNumber { get; private set; }
+    
+        private static object syncRoot = new Object();
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        private SerialNumberBase()
+        {
+            SerialNumber = 1;
+        }
+
+        /// <summary>
+        /// ПОлучаем единственный экземпляр.
+        /// </summary>
+        /// <returns></returns>
+        private static SerialNumberBase GetInst()
+        {
+            if (_instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (_instance == null)
+                        _instance = new SerialNumberBase();
+                }
+            }
+            return _instance;
+        }
+
+        /// <summary>
+        /// задает начальный номер
+        /// </summary>
+        /// <param name="startNumber"></param>
+        public static void SetStartNumber(int startNumber)
+        {
+            var inst = GetInst();
+           inst.SerialNumber = startNumber;
+        }
+
+        /// <summary>
+        /// Получает текущий порядковый номер. Делает инкримент порядкового номера
+        /// </summary>
+        /// <returns>Текущий порядковы й нмоер</returns>
+        public static int GetNumber()
+        {
+            var inst = GetInst();
+            var result = inst.SerialNumber;
+            inst.SerialNumber++;
+            return result;
+        }
+    }
+}
